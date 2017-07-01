64f8ab39a5805dc658ae1afa734706d84f2f2b70
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-07-01 11:08:15 +0530

    static method within package use without object

---
+++ Comedy/Comedy1.java
@@ -0,0 +1,7 @@
+package Comedy;
+
+public class Comedy1 {
+    static void check() {
+        System.out.println("This is working");
+    }
+}
---
+++ Comedy/Comedy2.java
@@ -0,0 +1,12 @@
+package Comedy;
+
+public class Comedy2 {
+    public static void main(String[] args) {
+        Comedy2 c2 = new Comedy2();
+        c2.check();
+    }
+
+    void check() {
+        Comedy1.check();
+    }
+}
