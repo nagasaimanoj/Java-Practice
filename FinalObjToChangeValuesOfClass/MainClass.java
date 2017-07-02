42657cf03e7826d23db728cfc23ff15a7b6ae416
nagasai <nagasai.g9@gmail.com>
2017-07-02 06:49:06 +0530

    Final Object changes instance values too..

---
+++ FinalObjToChangeValuesOfClass/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    public static void main(String[] args) {
+        final DataClass dc = new DataClass();
+        dc.a = 999;
+        System.out.println(dc.a);
+    }
+}
