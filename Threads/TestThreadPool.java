f5dc3bdd05021275f650a5fc8c1f9cee62acbc1e
nagasai.g9@gmail.com <saimanoj@KGFSL.COM>
2017-07-05 10:57:54 +0530

    small changes

--- Threads/TestThreadPool.java
+++ Threads/TestThreadPool.java
@@ -2,10 +2,9 @@ import java.util.concurrent.*;
 
 public class TestThreadPool implements Runnable {
     static int a = 0;
-    int j[] = { 9, 5, 3, 2, 8, 4, 0, 7, 6, 1 };
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
         for (int i = 0; i < 10; i++) {
             executor.execute(new TestThreadPool());
         }
@@ -13,6 +12,6 @@ public class TestThreadPool implements Runnable {
     }
 
     public void run() {
-        System.out.println(j[a++]);
+        System.out.println(++a);
     }
 }
