d0468804b7c15f0669f83da2d450e5d0fc086ad9
nagasai <nagasai.g9@gmail.com>
2017-07-11 07:13:56 +0530

    array checking

---
+++ Print Arrays/ArrayThings.java
@@ -0,0 +1,8 @@
+public class ArrayThings {
+    public static void main(String[] args) {
+        Integer a = new Integer(5);
+        System.out.println("a is " + a);
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(arr);
+    }
+}
