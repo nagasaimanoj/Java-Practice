d551bb22393edd28b41d3361ded29b596aa4455b
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-08-09 15:56:15 +0530

    small changes

---
+++ Maven Trails/Spring/src/main/java/com/manoj/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.manoj;
+
+public class HelloWorld {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
