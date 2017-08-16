e041585c78411ba456c536523163d935d381a558
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-08-16 10:54:29 +0530

    daily changes

--- Maven Trails/Spring/Sample/src/main/java/com/manoj/App.java
+++ Maven Trails/Spring/Sample/src/main/java/com/manoj/App.java
@@ -1,14 +1,9 @@
 package com.manoj;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"Spring-Module.xml");
 
-		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
-		obj.printHello();
+		((HelloWorld) new org.springframework.context.support.ClassPathXmlApplicationContext("Spring-Module.xml")
+				.getBean("helloBean")).printHello();
 	}
 }
--- Maven Trails/Spring/Sample/src/main/java/com/manoj/HelloWorld.java
+++ Maven Trails/Spring/Sample/src/main/java/com/manoj/HelloWorld.java
@@ -3,11 +3,15 @@ package com.manoj;
 public class HelloWorld {
 	private String name;
 
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String temp) {
+		this.name = temp;
+	}
+
+	public String getName() {
+		return this.name;
 	}
 
 	public void printHello() {
-		System.out.println("Hello ! " + name);
+		System.out.println("Hello ! " + this.name);
 	}
 }
