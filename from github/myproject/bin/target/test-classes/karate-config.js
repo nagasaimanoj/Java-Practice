e8275c3d6ed6dd7a93e0b1a921957b5874f70af8
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-08-24 19:19:15 +0530

    cleared mess

--- from github/myproject/bin/target/test-classes/karate-config.js
+++
@@ -1,9 +0,0 @@
-function() {
-  var env = karate.env || 'dev';
-  karate.log('karate.env system property was:', env);
-  var config = {
-    env: env,
-    myVarName: 'someValue'
-  }
-  return config;
-}
