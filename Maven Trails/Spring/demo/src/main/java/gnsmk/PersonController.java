84c3e427d183be46ac06bcd45be718d0acd0dc96
Naga Sai Manoj <nagasai.g9@gmail.com>
2017-09-15 19:10:53 +0530

    daily changes

--- Maven Trails/Spring/demo/src/main/java/gnsmk/PersonController.java
+++ Maven Trails/Spring/demo/src/main/java/gnsmk/PersonController.java
@@ -1,20 +1,19 @@
 package gnsmk;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @SpringBootApplication
-
 public class PersonController {
-	private static List<Person> personList = new ArrayList<Person>();
+	List<Person> allPersons = new ArrayList<Person>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonController.class, args);
@@ -22,13 +21,13 @@ public class PersonController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String Hello(Model model) {
-		model.addAttribute("list", personList);
+		model.addAttribute("list", allPersons);
 		return "personX.jsp";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String additem(@ModelAttribute("person") Person p) {
-		personList.add(p);
+		allPersons.add(p);
 		return "redirect:/";
 	}
 }
