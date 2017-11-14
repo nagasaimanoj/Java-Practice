5c12f38132d5bcf4a8c5d859b22b776e2d10ffa0
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-11-14 18:34:49 +0530

    every variable is seperately declared

--- RootFinder.java
+++ RootFinder.java
@@ -2,11 +2,16 @@ public class RootFinder {
 
     public static void main(String[] args) {
 
-        Double num = new Double(81), root = new Double(5), start = new Double(0), end = num, i, number, tmpVar,
-                result = (start + end) / 2;
-
-        for (i = 1.0; i < 1000000; i++) {
-
+        Double num = new Double(81);
+	Double root = new Double(5);
+	Double start = new Double(0);
+	Double end = num,
+	Double i = new Double(),
+	Double number = new Double(),
+	Double tmpVar = new Double(),
+        Double result = (start + end) / 2;
+	    
+        for (i = 1.0; i < 1000000.0; i++) {
             number = (start + end) / 2;
             result = number;
             tmpVar = Math.pow(number, root);
@@ -21,6 +26,6 @@ public class RootFinder {
 		}
 
         System.out.println(root + " root of " + num + " is " + result);
-		System.out.println("(" + result + ")^" + root + " = " + Math.pow(result, root));
+	System.out.println("(" + result + ")^" + root + " = " + Math.pow(result, root));
 	}
 }
