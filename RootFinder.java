4e4d36a8ebd7b041c2ebdab3849f40f4a3d4a619
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-11-14 18:38:37 +0530

    solved few syntax errors

--- RootFinder.java
+++ RootFinder.java
@@ -1,31 +1,31 @@
-public class RootFinder {
+public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Double num = new Double(81);
+    Double num = new Double(81);
 	Double root = new Double(5);
 	Double start = new Double(0);
-	Double end = num,
-	Double i = new Double(),
-	Double number = new Double(),
-	Double tmpVar = new Double(),
-        Double result = (start + end) / 2;
+	Double end = num;
+	Double i = new Double(0);
+	Double number = new Double(0);
+	Double tmpVar = new Double(0);
+    Double result = (start + end) / 2;
 	    
-        for (i = 1.0; i < 1000000.0; i++) {
-            number = (start + end) / 2;
-            result = number;
-            tmpVar = Math.pow(number, root);
-			if (tmpVar == num) {
-				result = number;
-				break;
-			} else if (tmpVar < num) {
-				start = (start + end) / 2;
-			} else{
-				end = (start + end) / 2;
-			}
+    for (i = 1.0; i < 1000000.0; i++) {
+        number = (start + end) / 2;
+        result = number;
+        tmpVar = Math.pow(number, root);
+		if (tmpVar == num) {
+			result = number;
+			break;
+		} else if (tmpVar < num) {
+			start = (start + end) / 2;
+		} else{
+			end = (start + end) / 2;
 		}
+	}
 
-        System.out.println(root + " root of " + num + " is " + result);
+    System.out.println(root + " root of " + num + " is " + result);
 	System.out.println("(" + result + ")^" + root + " = " + Math.pow(result, root));
 	}
 }
